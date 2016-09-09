bd51c75134646abd444f2af087ddce170c794341
zzjoseph <josephph14@gmail.com>
2016-09-09 09:58:48 -0400

    get /search

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const PORT = 8080;
 
 app.use(express.static(__dirname + '/app'));
 
+app.get('/search', function(req, res) {
+	console.log(req.query.q);
+});
+
 console.log("Spinning up server on port " + PORT)
 app.listen(PORT);
