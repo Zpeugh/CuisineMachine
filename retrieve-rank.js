713d0f4706b610b8fea6a825cb47cf7ba2f013de
Zpeugh <zach.peugh@spidasoftware.com>
2016-09-16 14:35:10 -0400

    Got the cluster set up

--- retrieve-rank.js
+++ retrieve-rank.js
@@ -8,11 +8,13 @@ console.log("Bootstrapping Retrieve and Rank engine");
 var retrieve_and_rank = new RetrieveAndRankV1({
   username: '0b8e9940-0890-41aa-9a0f-98b73077a868',
   password: 'nojwL5kgzQES',
+  use_vcap_services: false
 });
-// create cluster
+
+//
 // retrieve_and_rank.createCluster({
 //   cluster_size: '1',
-//   cluster_name: 'Cuisine Machine Recipe Cluster'
+//   cluster_name: 'Cuisine_Machine_Recipe_Cluster'
 // }, function (err, response) {
 //   if (err)
 //     console.log('error:', err);
@@ -23,26 +25,12 @@ var retrieve_and_rank = new RetrieveAndRankV1({
 // add document to cluster
 
 var solrClient = retrieve_and_rank.createSolrClient({
-  cluster_id: 'scdaa8d3b3_4828_4657_ac16_41b7f7510237',
-  collection_name: 'Cuisine Machine Recipe Cluster'
+  cluster_id: 'sc12b109cf_91ae_43fe_bb0e_15b68f0fd1cb',
+  collection_name: 'Cuisine_Machine_Recipe_Cluster'
 });
 
-
-//
-// retrieve_and_rank.listClusters({},
-//   function (err, response) {
-//     if (err)
-//       console.log('error:', err);
-//     else
-//       console.log(JSON.stringify(response, null, 2));
-// });
-
-//TODO: Add configuration files
-
-// retrieve_and_rank.uploadConfig({
-//   cluster_id: 'scdaa8d3b3_4828_4657_ac16_41b7f7510237',
-//   config_name: 'Retrieve and Rank Config',
-//   config_zip_path: './config/retreive_and_rank/r_and_r_config.zip'
+// retrieve_and_rank.deleteCluster({
+//   cluster_id: 'sc057a91b6_476f_43e5_91c8_2462b4c291a7'
 // },
 //   function (err, response) {
 //     if (err)
@@ -50,14 +38,29 @@ var solrClient = retrieve_and_rank.createSolrClient({
 //     else
 //       console.log(JSON.stringify(response, null, 2));
 // });
+
+
+//TODO: Add configuration files
 //
+// retrieve_and_rank.uploadConfig({
+//   cluster_id: 'sc12b109cf_91ae_43fe_bb0e_15b68f0fd1cb',
+//   config_name: 'retreiveAndRankConfig',
+//   config_zip_path: './config/retreive_and_rank/retreive_and_rank_config.zip'
+// },
+//   function (err, response) {
+//     if (err)
+//       console.log('error:', err);
+//     else
+//       console.log(JSON.stringify(response, null, 2));
+// });
+
 
 //TODO: add a collection to the cluster
-
+//
 // retrieve_and_rank.createCollection({
-//   cluster_id: 'scdaa8d3b3_4828_4657_ac16_41b7f7510237',
-//   config_name: 'Retrieve and Rank Config',
-//   collection_name: 'Cuisine Machine Recipe Cluster'
+//   cluster_id: 'sc12b109cf_91ae_43fe_bb0e_15b68f0fd1cb',
+//   config_name: 'retreiveAndRankConfig',
+//   collection_name: 'Cuisine_Machine_Recipe_Cluster'
 // },
 //   function (err, response) {
 //     if (err)
@@ -66,23 +69,40 @@ var solrClient = retrieve_and_rank.createSolrClient({
 //       console.log(JSON.stringify(response, null, 2));
 // });
 
+// retrieve_and_rank.listClusters({},
+//   function (err, response) {
+//     if (err)
+//       console.log('error:', err);
+//     else
+//       console.log(JSON.stringify(response, null, 2));
+// });
 
-var doc = { id : 1, title : 'Test', picture: 'www.url.com', ingredients: 'Anything; nothing',
-instructions: 'Do what you will', about: 'Whatever', yeild: '2 servings', tags: 'none'  };
-solrClient.add(doc, function(err) {
-  if(err) {
-    console.log('Error indexing document: ' + err);
-  } else {
-    console.log('Indexed a document.');
-    solrClient.commit(function(err) {
-      if(err) {
-        console.log('Error committing change: ' + err);
-      } else {
-        console.log('Successfully commited changes.');
-      }
-    });
-  }
-});
+
+
+//
+// var doc = { id : 1,
+//             title : 'Test',
+//             picture: 'www.url.com',
+//             ingredients: 'Anything nothing',
+//             instructions: 'Do what you will',
+//             about: 'Whatever',
+//             yield: '2 servings',
+//             tags: 'none'
+//         };
+// solrClient.add(doc, function(err) {
+//   if(err) {
+//     console.log('Error indexing document: ' + err);
+//   } else {
+//     console.log('Indexed a document.');
+//     solrClient.commit(function(err) {
+//       if(err) {
+//         console.log('Error committing change: ' + err);
+//       } else {
+//         console.log('Successfully commited changes.');
+//       }
+//     });
+//   }
+// });
 
 // query
 // var query = solrClient.createQuery();
@@ -102,7 +122,7 @@ solrClient.add(doc, function(err) {
 module.exports = function() {
     // query
     var query = solrClient.createQuery();
-    query.q({ '*' : '*' });
+    query.q({title: 'Test'});
     solrClient.search(query, function(err, searchResponse) {
       if(err) {
         console.log('Error searching for documents: ' + err);
