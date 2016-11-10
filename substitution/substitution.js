d35d3348d57c12303f03a059d226217bb31bb9cd
zzjoseph <josephphx14@gmail.com>
2016-11-10 18:12:39 -0500

    substitution

---
+++ substitution/substitution.js
@@ -0,0 +1,467 @@
+module.exports = {
+  "alcohol": {
+    amount: "any amount",
+    sub: "same amount of liquid water"
+  },
+  "allspice": {
+    amount: "1 tsp",
+    sub: "1/2 tsp cinnamon + 1/2 tsp ground cloves"
+  },
+  "almond paste": {
+    amount: "1-1/3 cups",
+    sub: "1-3/4 cups ground blanched almonds + 1-1/2 cups powdered sugar + 1 egg white + 1 tsp almond extract + 1/4 tsp salt"
+  },
+  "unshelled almonds": {
+    amount: "1 lb",
+    sub: "3/4 to 1 cup shelled"
+  },
+  "ammonium bicarbonate": {
+    amount: "3/4 tsp",
+    sub: "1 tsp baking soda"
+  },
+  "apple pie spice": {
+    amount: "1 tsp",
+    sub: "1/2 tsp ground cinnamon, 1/4 tsp ground ginger,1/8 tsp ground allspice and 1/8 tsp ground nutmeg"
+  },
+  "arrowroot": {
+    amount: "1 tbsp",
+    sub: "1 tbsp corn starch, potato starch, rice starch, OR 2 tbsp all purpose flour"
+  },
+  "baking mix": {
+    amount: "2 cups",
+    sub: "1-3/4 cups all-purpose flour, 2-1/2 tsp baking powder and 3/4 tsp salt."
+  },
+  "bisquick": {
+    amount: "2 cups",
+    sub: "1-3/4 cups all-purpose flour, 2-1/2 tsp baking powder and 3/4 tsp salt."
+  },
+  "baking powder": {
+    amount: "1 tsp",
+    sub: "1 tsp baking soda + 1/2 tsp ream of tartar"
+  },
+  "balsamic vinegar": {
+    amount: "1 tbsp",
+    sub: "1 tbsp sherry or cider vinegar"
+  },
+  "bay leaf": {
+    amount: "1 whole",
+    sub: "1/4 tsp crushed bay leaf"
+  },
+  "breadcrumbs": {
+    amount: "1/4 cup",
+    sub: "1/4 cup finely crushed cracker crumbs, corn flakes, or quick-cooking/old-fashioned oats"
+  },
+  "broth": {
+    amount: "1 cup",
+    sub: "1 tsp (or 1 cube) bouillon granules dissolved in 1 cup boiling water"
+  },
+  "buttermilk": {
+    amount: "1 cup",
+    sub: "1 tbsp lemon juice or white vinegar + enough milk to make 1 cup -OR- 1 tbsp vinegar + sweet milk to make 1 cup -OR- 2/3 cup plain yogurt + 1/3 cup sweet milk"
+  },
+  "butter": {
+    amount: "1 cup",
+    sub: "1 cup margarine -OR- 7/8 cup shortening and 1/2 tsp salt"
+  },
+  "capers": {
+    amount: "1 tbsp",
+    sub: "1 tbsp chopped green olives"
+  },
+  "carob powder": {
+    amount: "1 tbsp",
+    sub: "1 tbsp cocoa powder (either natural or Dutch-processed)"
+  },
+  "carob": {
+    amount: "3 tbsp",
+    sub: "3 tbsp cocoa and 1 tbsp shortening"
+  },
+  "chili sause": {
+    amount: "1 cup",
+    sub: "1 cup tomato sauce, 1/4 cup brown sugar, 2 tbsp vinegar, 1/4 tsp cinnamon, dash of ground cloves and dash of allspice"
+  },
+  "chives": {
+    amount: "2 tsp",
+    sub: "2 tsp green onion tops finely chopped"
+  },
+  "sweet baking chocolate": {
+    amount: "4 oz",
+    sub: "1/4 cup unsweetened cocoa powder + 1/3 cup sugar"
+  },
+  "semi-sweet chocolate": {
+    amount: "1 square (1 oz)",
+    sub: "3 tbsp chocolate chips -OR- 1 square (1oz) unsweetened chocolate + 1 tbsp sugar"
+  },
+  "unsweetened chocolate": {
+    amount: "1 oz",
+    sub: "3 tbsp cocoa + 1 tbsp butter or regular margarine or vegetable oil"
+  },
+  "semi-sweet chocolate chips": {
+    amount: "6 oz pkg",
+    sub: "1oz sweet cooking chocolate chopped"
+  },
+  "semi-sweet melted chocolate chips": {
+    amount: "6 oz pkg",
+    sub: "2 squares unsweetened chocolate, 2 tbsp shortening and 1/2 cup sugar"
+  },
+  "cocoa": {
+    amount: "1/4 cup or 4 tbsp",
+    sub: "1oz chocolate (decrease fat in recipe by 1/2 tbsp)"
+  },
+  "coffee": {
+    amount: "1/2 cup",
+    sub: "1/2 cup hot water and 1 tsp instant coffee granules"
+  },
+  "dark corn syrup": {
+    amount: "1 cup",
+    sub: "1 cup light corn syrup -OR- 3/4 cup light corn syrup + 1/4 cup molasses -OR- 1 cup maple-flavored syrup"
+  },
+  "light corn syrup": {
+    amount: "1 cup",
+    sub: "1 cups sugar + 1/4 cups water"
+  },
+  "corn starch": {
+    amount: "1 tbsp",
+    sub: "	2 tbsp all-purpose flour"
+  },
+  "clotted cream": {
+    amount: "1 cup",
+    sub: "1 cup crème fraîche -OR- 1 cup mascarpone cheese -OR- 1 cup heavy whipping cream (35%), whipped"
+  },
+  "coconut cream": {
+    amount: "1 cup",
+    sub: "1 cup whipping cream1 cup canned coconut cream -OR- 1 cup top layer of canned coconut milk (not low fat) (do not shake or stir can before skimming) -OR- 1 cup heavy whipping cream (35%) + 1/2 cup coconut cream powder"
+  },
+  "coffee cream": {
+    amount: "1 cup",
+    sub: "3 tbsp butter + 7/8 cup milk"
+  },
+  "crème fraîche cream": {
+    amount: "1 cup",
+    sub: "	1 cup sour cream -OR- 1 cup whipping cream + 1 tbsp buttermilk or yogurt -OR- ½ cup whipping cream + 1/2 cup sour cream -OR- 1 cup mascarpone cheese"
+  },
+  "heavy cream": {
+    amount: "1 cup",
+    sub: "3/4 cup milk and 1/3 cup butter or margarine (only for use in cooking and baking)"
+  },
+  "light cream": {
+    amount: "1 cup",
+    sub: "	3/4 cup milk and 3 tbsp butter or margarine (only for use in cooking and baking) -OR- 1 cup evaporated milk"
+  },
+  "whipping cream (40% fat)": {
+    amount: "1 cup",
+    sub: "1/3 cup butter + 3/4 cup milk"
+  },
+  "unwhipped cream": {
+    amount: "1 cup",
+    sub: "1 cup UNWHIPPED whipping cream expands to 2 cups when WHIPPED — I.e., if your recipe called for 1 cup of cream to make whipped cream, you could substitute 2 cups of an already whipped product"
+  },
+  "whipped cream": {
+    amount: "1 cup",
+    sub: "1 cup frozen (thawed) whipped topping -OR- 1 cup prepared whipped topping mix"
+  },
+  "cream of tartar": {
+    amount: "1/2 tsp",
+    sub: "1/2 tsp white vinegar or lemon juice"
+  },
+  "currant jelly": {
+    amount: "any amount",
+    sub: "rape jelly -OR- 3 parts apple jelly plus 1 part lemon juice -OR- muscadine jelly"
+  },
+  "currants": {
+    amount: "1 cup",
+    sub: "1 cup raisins -OR- 1 cup chopped dates -OR- 1 cup other dried fruit (cranberries, cherries, blueberries)"
+  },
+  "egg white": {
+    amount: "1",
+    sub: "2 tsp sifted dry egg white powder with 2 tbsp water"
+  },
+  "eggs": {
+    amount: "1 large",
+    sub: "2 egg whites -OR- 1/4 cup egg product -OR- 2 egg yolks + 1 tbsp water (for cookies and bars)"
+  },
+  "wheat flour": {
+    amount: "1 cup",
+    sub: "7/8 cup all purpose flour + 2 tbsp wheat germ -OR- 1 cup graham flour"
+  },
+  "cake flour": {
+    amount: "1 cup",
+    sub: "3/4 cup all purpose flour + 2 tbsp cornstarch"
+  },
+  "self rising flour": {
+    amount: "1 cup",
+    sub: "1 cup minus 2 tsp all purpose flour + 1-1/2 tsp baking powder + 1/4 tsp salt"
+  },
+  "garlic": {
+    amount: "1 medium clove",
+    sub: "1/8 tsp garlic powder -OR- 1/4 tsp instant minced garlic"
+  },
+  "ginger root": {
+    amount: "1 tsp",
+    sub: "3/4 tsp ground ginger"
+  },
+  "herbs": {
+    amount: "1 tbsp",
+    sub: "1 tsp dried herbs"
+  },
+  "honey": {
+    amount: "1 cup",
+    sub: "	1-1/4 cups sugar + 1/4 cup water or apple juice"
+  },
+  "italian seasonings": {
+    amount: "1-1/2 tsp",
+    sub: "	1/4 tsp each dried marjoram, oregano + basil plues 1/8 tsp sage"
+  },
+  "ketchup": {
+    amount: "1 cup",
+    sub: "1 cup tomato sauce, 1/2 cup sugar and 2 tbsp vinegar"
+  },
+  "leeks": {
+    amount: "1/2 cup",
+    sub: "1/2 cup sliced shallots or green onions"
+  },
+  "zest lemon": {
+    amount: "1 tsp",
+    sub: "1-2 tsp fresh grated lemon peel -OR- 1/2 tsp lemon extract"
+  },
+  "lemon juice": {
+    amount: "1 tbsp",
+    sub: "	1 tbsp bottled lemon juice or white vinegar"
+  },
+  "lemon": {
+    amount: "1 piece",
+    sub: "	3 tbsp lemon juice -OR- 1 tsp grated peel"
+  },
+  "zest lime": {
+    amount: "1 tsp",
+    sub: "1 tsp lemon or orange"
+  },
+  "lime juice": {
+    amount: "1 tbsp",
+    sub: "1 tbsp lemon or orange juice"
+  },
+  "maple syrup": {
+    amount: "1 cup",
+    sub: "	1 cup honey -OR- 3/4 cup corn syrup + 1/4 cup butter + 1/2 tsp maple extract (optional)"
+  },
+  "miniature marshmallows": {
+    amount: "1 cup",
+    sub: "10 large marshmallows"
+  },
+  "marzipan": {
+    amount: "2-1/2 cups",
+    sub: "2 cups almond paste + 1 cup powdered sugar + 2 tbsp light corn syrup"
+  },
+  "mascarpone cheese": {
+    amount: "1 cup",
+    sub: "	1 cup Crème fraîche -OR- 3/4 cup cream cheese beaten with 1/4 cup heavy whipping cream (35%)"
+  },
+  "mayonnaise": {
+    amount: "1 cup",
+    sub: "1 cup sour cream -OR- 1 cup yogurt — For use in salads and salad dressings"
+  },
+  "sour milk": {
+    amount: "1 cup",
+    sub: "1 cup plain yogurt -OR- 1 cup whole milk and 1 tbsp lemon juice"
+  },
+  "coconut milk": {
+    amount: "1 cup",
+    sub: "1 cup canned coconut milk (not low fat) -OR- 3 tbsp canned cream of coconut + enough hot water or milk to equal 1 cup -OR- 1/4 cup coconut cream powder + 1 cup hot water or milk"
+  },
+  "evaporated milk": {
+    amount: "1 cup",
+    sub: "1 cup half & half (10-12%) -OR- 1 cup light or table cream (20%) -OR- 1 cup light (30%) or heavy whipping cream (35%)"
+  },
+  "nut milk": {
+    amount: "1 cup",
+    sub: "1 cup cow’s milk"
+  },
+  "rice milk": {
+    amount: "1 cup",
+    sub: "1 cup cow’s milk"
+  },
+  "soy milk": {
+    amount: "1 cup",
+    sub: "1 cup cow’s milk"
+  },
+  "sweetened milk": {
+    amount: "1 can",
+    sub: "Heat and stir 1/3 cup and 2 tbsp evaporated milk, 1 cup sugar and 3 tbsp butter until sugar has dissolved."
+  },
+  "milk": {
+    amount: "1 cup",
+    sub: "1/2 cup evaporated milk + 1/2 cup water -OR- nonfat dry milk prepared as directed on package"
+  },
+  "molasses": {
+    amount: "1 cup",
+    sub: "1/2 to 1 cup honey (flavor will be milder) -OR- 1 cup dark corn syrup -OR- 1 cup maple syrup -OR- 3/4 cup light or dark brown sugar heated to dissolve in 1/4 cup liquid"
+  },
+  "mushrooms": {
+    amount: "1 cup",
+    sub: "1 (4oz) can mushroom stems and pieces, drained"
+  },
+  "mustard": {
+    amount: "1 tsp",
+    sub: "1 tbsp prepared mustard"
+  },
+  "oil": {
+    amount: "1 cup",
+    sub: "1 cup applesauce; for cake and cookie recipes"
+  },
+  "onion": {
+    amount: "1 small",
+    sub: "1 tbsp instant minced onion — Dried onion may be added directly to moist foods such as soups, gravies, sauces and salad dressings. You may need to rehydrate it with a little water before adding it to drier food"
+  },
+  "zest orange": {
+    amount: "1 tsp",
+    sub: "1 tsp dried orange peel -OR- 1 tsp lemon or lime zest -OR- 1 tsp finely chopped candied orange peel"
+  },
+  "orange juice": {
+    amount: "1 cup",
+    sub: "1 cup reconstituted frozen concentrate"
+  },
+  "orange": {
+    amount: "1 piece",
+    sub: "1/3 cup juice -OR- 2 tsp grated peel"
+  },
+  "polenta": {
+    amount: "1 cup",
+    sub: "1 cup cornmeal -OR- 1 cup maize meal -OR- 1 cup corn grits"
+  },
+  "poultry seasoning": {
+    amount: "1 tsp",
+    sub: "1/4 tsp ground thyme + 3/4 tsp ground sage"
+  },
+  "pumpkin pie spice": {
+    amount: "1 tsp",
+    sub: "Mix 1/2 tsp ground cinnamon, 1/4 tsp ground ginger, 1/8 tsp ground allspice and 1/8 tsp ground nutmeg"
+  },
+  "raisins": {
+    amount: "1 cup",
+    sub: "1 cup dried currants -OR- 1 cup pitted chopped dates -OR- 1 cup dried fruit, finely chopped (cherries, cranberries, blueberries, etc.)"
+  },
+  "red pepper sause": {
+    amount: "3 or 4 drops",
+    sub: "1/8 tsp ground red pepper (cayenne)"
+  },
+  "ricotta cheese": {
+    amount: "1 cup",
+    sub: "1 cup cottage cheese + 1 Tbsp skim milk"
+  },
+  "rose water": {
+    amount: "1 tsp",
+    sub: "1 tsp any flower-based water"
+  },
+  "rum": {
+    amount: "any amount",
+    sub: "1 part rum extract + 3 parts water — I.e.: for 1/4 cup rum, substitute 1 tbsp rum extract + 3 tbsp water."
+  },
+  "saffron": {
+    amount: "1 tsp",
+    sub: "1 tsp tumeric"
+  },
+  "sesame seed": {
+    amount: "1 tbsp",
+    sub: "1 tbsp finely chopped blanched almonds"
+  },
+  "melted shortening": {
+    amount: "1 cup",
+    sub: "1 cup cooking oil"
+  },
+  "solid shortening": {
+    amount: "1 cup",
+    sub: "1-1/8 cup butter or margarine (decrease salt in recipe by 1/2 tsp)"
+  },
+  "sour cream": {
+    amount: "1 cup",
+    sub: "1 cup yogurt -OR- mix together 3/4 cup milk, 3/4 tsp lemon juice and 1/2 cup butter"
+  },
+  "soy sauce": {
+    amount: "1 tbsp",
+    sub: "1 tbsp teriyaki Sauce"
+  },
+  "brown sugar": {
+    amount: "1 cup",
+    sub: "3/4 cup granulated sugar + 1/4 cup molasses"
+  },
+  "powdered sugar": {
+    amount: "1 cup",
+    sub: "1 cup granulated sugar + 1 tbsp cornstarch — Process in a food processor using the metal blade attachment until it’s well blended and powdery."
+  },
+  "maple sugar": {
+    amount: "1/2 cup",
+    sub: "1 cup maple syrup (reduce liquid in recipe by 1/4 cup) -OR- 3/4 cup granulated white sugar + 1 tsp pure maple extract"
+  },
+  "sugar": {
+    amount: "1 cup",
+    sub: "1 cup light brown sugar (packed) or 2 cups powdered sugar"
+  },
+  "tomato juice": {
+    amount: "1 cup",
+    sub: "	1/2 cup tomato sauce + 1/2 cup water"
+  },
+  "tomato paste": {
+    amount: "1/2 cup",
+    sub: "1 cup tomato sauce cooked uncovered to reduce to 1/2 cup"
+  },
+  "tomato sauce": {
+    amount: "1 cup",
+    sub: "tbsp tomato paste and 1/2 water"
+  },
+  "tomato soup": {
+    amount: "10-3/4 oz can",
+    sub: "1 cup tomato sauce + 1/4 cup water"
+  },
+  "tomatoes": {
+    amount: "1 cup",
+    sub: "about 1-1/3 cups cut-up fresh tomatoes, simmered 10 minutes"
+  },
+  "treacle": {
+    amount: "1 cup",
+    sub: "1 cup light or dark corn syrup -OR- 1 cup light molasses"
+  },
+  "apple cider vinegar": {
+    amount: "1/4 cup",
+    sub: "1/4 cup white vinegar"
+  },
+  "white vinegar": {
+    amount: "1/4 cup",
+    sub: "1/4 cup apple cider vinegar -OR- 1/3 cup lemon juice"
+  },
+  "unshelled walnuts": {
+    amount: "1 lb",
+    sub: "1-1/2 to 1-3/4 cups shelled"
+  },
+  "water": {
+    amount: "3/4 cup",
+    sub: "1 cup cow’s milk"
+  },
+  "red wine": {
+    amount: "1 cup",
+    sub: "1 cup apple cider, grape juice or cranberry juice"
+  },
+  "white wine": {
+    amount: "1 cup",
+    sub: "1 cup apple juice, apple cider, white grape juice or chicken broth"
+  },
+  "wine in desserts": {
+    amount: "1/2 cup",
+    sub: "1/2 cup fruit juice"
+  },
+  "wine in savory recipes": {
+    amount: "1/2 cup",
+    sub: "1/2 cup chicken broth"
+  },
+  "compressed yeast": {
+    amount: "3/5 oz",
+    sub: "1 package (1/4oz) active dry yeast -OR- 2-1/2 tsp loose active dry yeast"
+  },
+  "yeast": {
+    amount: "1 (1/4 oz) pkg",
+    sub: "2-1/4 tsp regular or quick active dry -OR- 1 (o.6oz) compressed cake yeast"
+  },
+  "yogurt": {
+    amount: "1 cup",
+    sub: "1 cup buttermilk -OR- 1 cup sour cream"
+  }
+
+}
