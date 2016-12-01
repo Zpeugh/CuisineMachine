f1703abe561d88b253adf725010514f046ef1abf
zzjoseph <josephphx14@gmail.com>
2016-11-30 20:25:08 -0500

    nlc sub

--- nlc/classifier.js
+++ nlc/classifier.js
@@ -9,22 +9,22 @@ var natural_language_classifier = new NaturalLanguageClassifierV1({
   username: "ea81bd9c-8673-474d-9db1-4eba97f2d14f"
 });
 
-var id = '8d6ce2x125-nlc-34';
-// 8d6f17x127-nlc-37
+var id = 'b73235x135-nlc-29';
+// b73235x135-nlc-29
 
 // list all classfier
-// natural_language_classifier.list({},
-//   function(err, response) {
-//     if (err)
-//         console.log('error:', err);
-//       else
-//         console.log(JSON.stringify(response, null, 2));
-//   }
-// );
+natural_language_classifier.list({},
+  function(err, response) {
+    if (err)
+        console.log('error:', err);
+      else
+        console.log(JSON.stringify(response, null, 2));
+  }
+);
 
 // check status of the classifier
 natural_language_classifier.status({
-  classifier_id: "8d6ce2x125-nlc-34" },
+  classifier_id: "b73235x135-nlc-29" },
   function(err, response) {
     if (err)
       console.log('error:', err);
@@ -46,7 +46,7 @@ natural_language_classifier.status({
 
 
 //
-// natural_language_classifier.remove({ classifier_id: '8d6f17x127-nlc-43' }, function(err, response) {
+// natural_language_classifier.remove({ classifier_id: '8d6f17x127-nlc-37' }, function(err, response) {
 //     if (err)
 //       console.log('error:', err);
 //     else
